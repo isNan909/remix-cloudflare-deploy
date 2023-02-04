5a9546ca1a269ea0593f8690aec19afd97841946
ishan-me <ishan02016@gmail.com>
2023-02-04 15:06:33 +0545

    preview deploy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Remix with Cloudflare Pages!</h1>
+      <h1>Remix with Cloudflare Preview Pages!</h1>
     </div>
   );
 }
